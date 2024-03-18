3cf28640c96daaa342374e0d73f8fd3e122ddf16
pawelorgana <151195356+pawelorgana@users.noreply.github.com>
2024-03-18 13:15:12 +0100

    Update SIMPLE JOIN + CONCAT.sql

--- SIMPLE JOIN + CONCAT.sql
+++ SIMPLE JOIN + CONCAT.sql
@@ -2,3 +2,5 @@ SELECT O.* ,Companyname, ContactName , Phone, CONCAT(Address, ' ' , City) AS Cit
 FROM Customers AS C
 Join Orders AS O
 ON C.CustomerID = O.CustomerID
+WHERE EmployeeID Between 6 AND 8
+ORDER BY CustomerID

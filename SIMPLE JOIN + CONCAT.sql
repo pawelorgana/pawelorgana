b67860a5cc485aab333a6611fba908f5efcf235d
pawelorgana <151195356+pawelorgana@users.noreply.github.com>
2024-03-18 13:51:09 +0100

    Update SIMPLE JOIN + CONCAT.sql

--- SIMPLE JOIN + CONCAT.sql
+++ SIMPLE JOIN + CONCAT.sql
@@ -1,3 +1,7 @@
+/* only for learning purpose 
+based on https://github.com/microsoft/sql-server-samples/blob/master/samples/databases/northwind-pubs/instnwnd.sql 
+*/
+-- MSSQL
 SELECT O.* ,Companyname, ContactName , Phone, CONCAT(Address, ' ' , City) AS City_Address
 FROM Customers AS C
 Join Orders AS O
